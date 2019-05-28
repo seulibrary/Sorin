b8d41d80fd3f3119a1574497574b488caeaaeaef
akosarek <akosarek@gmail.com>
2019-05-28 14:15:37 -0500

    move files to branch

--- apps/frontend/assets/js/actions/collection.js
+++ apps/frontend/assets/js/actions/collection.js
@@ -7,16 +7,10 @@ export const getCollection = (url) => (dispatch) => {
         type: Constants.GETTING_COLLECTION_BY_URL
     })
 
-<<<<<<< HEAD
     fetch(apiUrl + "/api/collection/" + url, {
         method: "GET",
         credentials: "same-origin"
     }).then(parseJSON)
-=======
-    fetch(hostUrl + ":" + port + "/api/collection/" + url, {
-        method: "GET",
-    }).then(parseJson)
->>>>>>> 99cc54d... convert api/collection to GET and use built-in resources to define functions called in controller.
         .then((json) => {
             dispatch({
                 type: Constants.GET_COLLECTION_BY_URL,
