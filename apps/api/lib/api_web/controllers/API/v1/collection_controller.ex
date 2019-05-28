2b24f02a766d76c2f6907940505308e5178d68cd
akosarek <akosarek@gmail.com>
2019-05-28 14:15:37 -0500

    move files to branch

--- apps/api/lib/api_web/controllers/API/v1/collection_controller.ex
+++ apps/api/lib/api_web/controllers/API/v1/collection_controller.ex
@@ -11,7 +11,8 @@ defmodule ApiWeb.V1.CollectionController do
       nil -> 
         conn
         |> put_status(404)
-        |> render(ErrorView, "404.json")
+        |> put_view(ErrorView)
+        |> render("404.json")
 
       collection ->
         conn
@@ -57,36 +58,4 @@ defmodule ApiWeb.V1.CollectionController do
     |> put_status(400)
     |> render(ErrorView, "400.json")
   end
-
-  #TODO: Waiting on Casey to merge his changes in before continued work to reduce any duplicate work or changes
-
-#  def update(conn, %{"id" => id, "collection" => collection} = _params) do
-#    IO.inspect _params
-#
-#    with collection_struct when is_map(collection_struct) <- Collections.get_collection!(id),
-#         {:ok, updated_collection} <- Collections.update_collection(collection_struct, collection) do
-#
-#
-#      ## need way to update colelction with one call? Or should I do what channel does? Can structs be nested?
-#
-#
-#      FrontendWeb.Endpoint.broadcast!(
-#        "collection:#{id}",
-#        "updated_collection",
-#        ApiWeb.CollectionView.render("dashboardCollection.json", collection: updated_collection)
-#      )
-#
-#      conn
-#      |> put_view(CollectionView)
-#      |>render("collection.json", collection: updated_collection)
-#    end
-#  end
-#
-#  def delete(conn, %{"id" => id}) do
-#    collection = Collections.get_collection!(id)
-#
-#    with {:ok, %Collections.Collection{}} <- Collections.delete_collection(collection) do
-#      send_resp(conn, :no_content, "")
-#    end
-#  end
 end
