a68a63c482f11148f0518c0bea96714661d40bcf
akosarek <akosarek@gmail.com>
2019-06-05 11:13:53 -0500

    remove actual user id

--- apps/api/lib/api_web/views/user_view.ex
+++ apps/api/lib/api_web/views/user_view.ex
@@ -3,11 +3,10 @@ defmodule ApiWeb.UserView do
     alias Core.Accounts.User
 
     def render("user.json", %User{} = user) do
-      %{
-        id: user.id,
-        fullname: user.fullname,
-        email: user.email,
-        photo_url: user.photo_url
-      }
+        %{
+            fullname: user.fullname,
+            email: user.email,
+            photo_url: user.photo_url
+        }
     end
 end
