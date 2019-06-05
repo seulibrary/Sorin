2668bc1611b0f9648589ed34f7f3007892cedd44
akosarek <akosarek@gmail.com>
2019-06-05 11:14:00 -0500

    update router

--- apps/api/lib/api_web/router.ex
+++ apps/api/lib/api_web/router.ex
@@ -5,15 +5,22 @@ defmodule ApiWeb.Router do
   pipeline :api do
     plug :accepts, ["json"]
     plug :fetch_session
+  end
+  
+  pipeline :auth do
     plug ApiWeb.Auth
   end
 
   scope "/api", ApiWeb do
     pipe_through [:api]
 
-    resources "/collection", CollectionController, only: [:show]
+    post "/token", V1.TokenController, :manage_tokens
 
-    # post "/collection", CollectionController, :get_collection
-    post "/file/:id", FileController, :get_file
+    scope "/" do
+      pipe_through [:auth]
+
+      resources "/collection", V1.CollectionController, only: [:create]
+      post "/file/:id", FileController, :get_file
+    end
   end
 end
