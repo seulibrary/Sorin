bf53235f8ef7da833568b8ebfc31f341969cdc9b
akosarek <akosarek@gmail.com>
2019-06-05 09:53:39 -0500

    remove guardian

--- apps/api/lib/api_web/controllers/guardian/authpipeline.ex
+++
@@ -1,9 +0,0 @@
-defmodule ApiWeb.Guardian.AuthPipeline do
-  use Guardian.Plug.Pipeline, otp_app: :api,
-  module: ApiWeb.Guardian,
-  error_handler: ApiWeb.AuthErrorHandler
-
-  plug Guardian.Plug.VerifyHeader, realm: "Bearer"
-  plug Guardian.Plug.EnsureAuthenticated
-  plug Guardian.Plug.LoadResource
-end
