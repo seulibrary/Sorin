40df16b9fabd917193d7b678066408ea53e53470
Casey Gibbs <caseygibbs@gmail.com>
2019-06-12 15:11:55 -0500

    Update Collections schema:
    - Add field :chain_of_cust (array of strings), which records the
      complete history for a given collection of who has had write_access
      to it and when
    
    - Rename :provenance to :import_stamp, since :provenance is too
      similar in meaning to "chain of custody"
    
    - Update many_to_many join with new CollectionUser schema
    
    - Remove :type from timestamps()

--- apps/core/lib/collections/collection.ex
+++ apps/core/lib/collections/collection.ex
@@ -2,31 +2,31 @@ defmodule Core.Collections.Collection do
   use Ecto.Schema
   import Ecto.Changeset
 
-
   schema "collections" do
+    field :chain_of_cust, {:array, :string}
     field :clones_count, :integer, default: 0
-    field :creator_id, :id
     field :imports_count, :integer, default: 0
+    field :import_stamp, :string # To record timestamp and creator on import
     field :permalink, :string
-    field :provenance, :string # To record timestamp and creator on import
     field :published, :boolean, default: false
     field :tags, {:array, :string}, default: []
     field :title, :string
-    field :write_users, {:array, :string} # Records who has write access
+    field :write_users, {:array, :string} # Fullnames of users w/write access
+    field :creator_id, :id
 
     has_one      :notes,     Core.Notes.Note
     has_many     :files,     Core.Files.File
     has_many     :resources, Core.Resources.Resource
     many_to_many :users,     Core.Accounts.User,
-      join_through: Core.Collections.CollectionsUsers
+      join_through: Core.CollectionsUsers.CollectionUser
 
-    timestamps(type: :utc_datetime)
+    timestamps()
   end
 
   @doc false
   def changeset(collection, attrs) do
     collection
-    |> cast(attrs, [:clones_count, :creator_id, :imports_count, :permalink, :provenance, :published, :tags, :title, :write_users])
+    |> cast(attrs, [:chain_of_cust, :clones_count, :creator_id, :imports_count, :import_stamp, :permalink, :published, :tags, :title, :write_users])
     |> validate_required([:permalink, :title])
   end
 end
