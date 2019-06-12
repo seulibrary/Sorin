07bf3bcdcd53bebb68db7866f24e60b0806f062e
Casey Gibbs <caseygibbs@gmail.com>
2019-06-12 16:06:44 -0500

    Update File schema:
    - Remove field :uploader_id
    
    - Replace with field :user_id
    
    - Remove :type field on timestamps()

--- apps/core/lib/files/file.ex
+++ apps/core/lib/files/file.ex
@@ -2,26 +2,24 @@ defmodule Core.Files.File do
   use Ecto.Schema
   import Ecto.Changeset
 
-
   schema "files" do
     field :file_url, :string
     field :media_type, :string
     field :size, :integer
     field :title, :string
-    field :uploader_id, :id
     field :uuid, :string
 
     belongs_to :collection, Core.Collections.Collection
     belongs_to :resource,   Core.Resources.Resource
-    
-    timestamps(type: :utc_datetime)
+    belongs_to :user,       Core.Accounts.User
+
+    timestamps()
   end
 
   @doc false
   def changeset(file, attrs) do
     file
-    |> cast(attrs, [:file_url, :media_type, :size, :title,
-		   :uploader_id, :uuid, :collection_id, :resource_id])
-    |> validate_required([:title, :uuid, :uploader_id])
+    |> cast(attrs, [:file_url, :media_type, :size, :title, :uuid, :collection_id, :resource_id, :user_id])
+    |> validate_required([:title, :uuid])
   end
 end
