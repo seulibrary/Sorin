0bc309308e2c30b9d37580564b2e6be0c1d7676f
akosarek <akosarek@gmail.com>
2019-06-13 15:48:44 -0500

    add user_id back into view

--- apps/api/lib/api_web/views/user_view.ex
+++ apps/api/lib/api_web/views/user_view.ex
@@ -3,10 +3,11 @@ defmodule ApiWeb.UserView do
     alias Core.Accounts.User
 
     def render("user.json", %User{} = user) do
-        %{
-            fullname: user.fullname,
-            email: user.email,
-            photo_url: user.photo_url
-        }
+      %{
+        id: user.id,
+        fullname: user.fullname,
+        email: user.email,
+        photo_url: user.photo_url
+      }
     end
 end
