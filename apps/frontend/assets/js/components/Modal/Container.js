c731d74b8a67f999594c603e0f178fd31af025e9
akosarek <akosarek@gmail.com>
2019-06-13 15:39:36 -0500

    remove unused conditional for modal

--- apps/frontend/assets/js/components/Modal/Container.js
+++ apps/frontend/assets/js/components/Modal/Container.js
@@ -112,11 +112,9 @@ class Modals extends Component {
 
     render() {
         const modals = this.props.modals.modals.map((item,i) => {
-            if (item.panel) {
+           
                 return <ModalPortal key={i} ><Modal item={item} onClose={(item) => this.props.dispatch(closeModal(item))}/></ModalPortal>
-            } else {
-                return <PermaLinkModalPortal key={i} ><Modal item={item} onClose={(item) => this.props.dispatch(closeModal(item))}/></PermaLinkModalPortal>
-            }
+
         })
 
         return (
