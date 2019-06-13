f678ba124e7d908a505f767aa264a9ff03075ed8
akosarek <akosarek@gmail.com>
2019-06-13 15:45:57 -0500

    add auth_tokens

---
+++ apps/core/lib/auth_token/auth_token.ex
@@ -0,0 +1,25 @@
+defmodule Core.AuthTokens.AuthToken do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @derive {Jason.Encoder, only: [:token, :use_count, :label]}
+
+  schema "auth_tokens" do
+    field :token, :map
+    field :use_count, :integer, default: 0
+    field :type, :string
+    field :label, :string
+
+    belongs_to :user, Core.Accounts.User
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(auth_token, attrs) do
+    auth_token
+    |> cast(attrs, [:token, :type, :user_id, :use_count, :label])
+    |> validate_required([:token, :type, :user_id])
+    |> unique_constraint(:token)
+  end
+end
