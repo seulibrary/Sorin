07e8fc2b888a90f26f195458c02cac1f65d08040
akosarek <akosarek@gmail.com>
2019-06-13 14:55:08 -0500

    controller to generate api tokens

--- apps/api/lib/api_web/controllers/API/v1/token_controller.ex
+++ apps/api/lib/api_web/controllers/API/v1/token_controller.ex
@@ -1,25 +1,35 @@
 defmodule ApiWeb.V1.TokenController do
   use ApiWeb, :controller
 
-  alias Core.Auth
+  alias Core.AuthTokens
 
-  def create_token(conn, %{"user" => user}) do
+  def index(conn, %{"user" => user}) do
+    case Phoenix.Token.verify(conn, "user_id", user) do
+      {:ok, user_id} ->
+
+        conn
+        |> put_status(200)
+        |> render(ApiWeb.API.TokenView, data: AuthTokens.get_auth_tokens_by_user_id(user_id))
+    end
+  end
+
+  def create(conn, %{"user" => user}) do
     case Phoenix.Token.verify(conn, "user_id", user) do
       {:ok, user_id} ->
         token = Phoenix.Token.sign("ElNO1SCiwmTp7Oxd9gkkv77FQutRdSMOTJvF8UBvT7hW2HrxZorkiPiXYY0xSmFN", "user_id", user_id)
 
-        Auth.create_auth_token(%{
-              token: token,
+        with {:ok, %AuthTokens.AuthToken{}} <- AuthTokens.create_auth_token(%{
+              token: %{key: token},
               user_id: user_id,
-              type: "api"})
+              type: "api"}) do
+          conn
+          |> put_status(200)
+          |> json(%{token: token})
+        end
 
+      {:error, _reason} ->
         conn
-        |> json(%{token: token})
-
-      {:error, reason} ->
-        conn
-        |> put_status(400)
-        |> json(%{error: reason})
+        |> put_status(:unauthorized)
 
       _ ->
         conn
@@ -28,48 +38,26 @@ defmodule ApiWeb.V1.TokenController do
     end
   end
 
-  def delete_token(conn, %{"token" => token}) do
-    conn
-#    Auth.delete_auth_token(
-#      Auth.get_auth_token!(token)
-#    )
-  end
-
-  def manage_tokens(conn, %{"user" => user}) do
-    conn
-# case Phoenix.Token.verify(conn, "user_id", user) do
-# :ok, user_id} ->
-#  secret_key = Phoenix.Token.sign(ApiWeb.Endpoint, "Punch it.", user_id, key_length: 256)
-#
-#  {_status, token, claims} = Guardian.encode_and_sign(%{id: user}, %{})
-#
-#  Core.Accounts.update_user(
-#    Core.Repo.get_by!(Core.Accounts.User, id: user_id),
-#    %{token: secret_key})
-#
-#  conn
-#  |> json(%{token: token, claims: claims})
-#  ->
-#  conn
-#  |> put_status(400)
-#  |> json(%{message: "ERROR: User Not Verified"})
-#
-  end
-
-  def manage_tokens(conn, %{"token" => token, "refresh" => true}) do
-    conn
- #   case ApiWeb.Guardian.refresh(token) do
- #     {:ok,  _old_stuff, {new_token, new_claims}} ->
- #       conn
- #       |> json(%{new_token: new_token, claims: new_claims})
- #     {:error, reason} ->
- #       conn
- #       |> put_status(400)
- #       |> json(%{error: reason})
- #   end
-  end
-
-  def refresh_token() do
-    {:ok, "refreshed"}
+  def delete(conn, %{"user" => user, "id" => id}) do
+    case Phoenix.Token.verify(conn, "user_id", user) do
+      {:ok, _user_id} ->
+        #TODO: Move the Core.Repo function to AuthTokens and create a function.
+        #TODO: Talk with Casey on new schema / table for auth_tokens for his branch for merge.
+        #TODO: Token table should also have a label field for human readabiliy.
+        with token when is_map(token)  <- AuthTokens.get_auth_token(%{key: id}),
+             {:ok, %AuthTokens.AuthToken{}} <- AuthTokens.delete_auth_token(token) do
+          conn
+          |> put_status(200)
+          |> json(%{message: "Token deleted."})
+        else
+          _ ->
+            conn
+            |> put_status(400)
+            |> json(%{message: "Token not deleted."})
+        end
+      {:error, _} ->
+        conn
+        |> put_status(:unauthorized)
+    end
   end
 end
