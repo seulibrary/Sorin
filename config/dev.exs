30ff2ddda9ccd422a85297a0d504dacbadadb208
Casey Gibbs <caseygibbs@gmail.com>
2019-07-09 12:33:31 -0500

    Import stanzas from api/config/dev.exs to main config

--- config/dev.exs
+++ config/dev.exs
@@ -8,3 +8,28 @@ config :core, Core.Repo,
   database: "core_dev",
   hostname: "localhost",
   pool_size: 10
+
+################ API ####################
+# For development, we disable any cache and enable
+# debugging and code reloading.
+#
+# The watchers configuration can be used to run external
+# watchers to your application. For example, we use it
+# with webpack to recompile .js and .css sources.
+config :api, ApiWeb.Endpoint,
+  http: [port: 8080],
+  debug_errors: true,
+  code_reloader: true,
+  check_origin: false,
+  watchers: []
+
+################ META ####################
+# Do not include metadata nor timestamps in development logs
+config :logger, :console, format: "[$level] $message\n"
+
+# Initialize plugs at runtime for faster development compilation
+config :phoenix, :plug_init_mode, :runtime
+
+# Set a higher stacktrace during development. Avoid configuring such
+# in production as building large stacktraces may be expensive.
+config :phoenix, :stacktrace_depth, 20
