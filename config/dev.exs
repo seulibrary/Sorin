2bfbeedab7cc13b75e8817181e00aaa85b8706ec
Casey Gibbs <caseygibbs@gmail.com>
2019-07-09 12:53:17 -0500

    Import stanzas from frontend/config/dev.exs to main config

--- config/dev.exs
+++ config/dev.exs
@@ -23,6 +23,39 @@ config :api, ApiWeb.Endpoint,
   check_origin: false,
   watchers: []
 
+################ FRONTEND ####################
+# For development, we disable any cache and enable
+# debugging and code reloading.
+#
+# The watchers configuration can be used to run external
+# watchers to your application. For example, we use it
+# with webpack to recompile .js and .css sources.
+config :frontend, FrontendWeb.Endpoint,
+  http: [port: 4000],
+  debug_errors: true,
+  code_reloader: true,
+  check_origin: false,
+  watchers: [
+      node: [
+      "node_modules/webpack/bin/webpack.js",
+      "--watch-stdin",
+      "--color",
+      "--config",
+      "webpack.config.js",
+      cd: Path.expand("../assets", __DIR__)]
+  ]
+
+# Watch static and templates for browser reloading.
+config :frontend, FrontendWeb.Endpoint,
+  live_reload: [
+    patterns: [
+      ~r{priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$},
+      ~r{priv/gettext/.*(po)$},
+      ~r{lib/frontend_web/views/.*(ex)$},
+      ~r{lib/frontend_web/templates/.*(eex)$}
+    ]
+  ]
+
 ################ META ####################
 # Do not include metadata nor timestamps in development logs
 config :logger, :console, format: "[$level] $message\n"
