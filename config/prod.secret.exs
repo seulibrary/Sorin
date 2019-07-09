7607a6dd16317f51fb30cfce9a537a07d4848ca0
Casey Gibbs <caseygibbs@gmail.com>
2019-07-09 13:56:14 -0500

    Move secrets from sorin.exs to env variables

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -17,13 +17,6 @@ config :core, Core.Repo,
   url: database_url,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
-secret_key_base =
-  System.get_env("SECRET_KEY_BASE") ||
-    raise """
-    environment variable SECRET_KEY_BASE is missing.
-    You can generate one by calling: mix phx.gen.secret
-    """
-
 ################ API ####################
 ueberauth_client_id =
   System.get_env("UEBERAUTH_CLIENT_ID") ||
