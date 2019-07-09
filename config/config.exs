f0d8feb9f7c36d9d3e63d4ecdd329491ba22843f
Casey Gibbs <caseygibbs@gmail.com>
2019-07-09 13:27:45 -0500

    Relocate sorin.exs to Sorin root

--- config/config.exs
+++ config/config.exs
@@ -53,7 +53,7 @@ config :logger, :console,
 config :phoenix, :json_library, Jason
 
 # Import configuration from sorin.exs
-import_config "sorin.exs"
+import_config "../sorin.exs"
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
