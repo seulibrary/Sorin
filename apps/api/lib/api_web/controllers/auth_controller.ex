46acac91b27adcbe6a84b412f5003c1d0336fc73
Alex <akosarek@stedwards.edu>
2019-07-23 13:54:47 -0500

    add state so on redirect for auth state persists for user

--- apps/api/lib/api_web/controllers/auth_controller.ex
+++ apps/api/lib/api_web/controllers/auth_controller.ex
@@ -16,12 +16,12 @@ defmodule ApiWeb.AuthController do
       |> redirect(to: "/")
     end
 
-    def callback(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do
+    def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
       fullname = auth.info.first_name <> " " <> auth.info.last_name
       user_params = %{fullname: fullname, email: auth.info.email}
       changeset = User.changeset(%User{}, user_params)
-
-      create(conn, changeset)
+  
+      create(conn, changeset, params["state"])
     end
 
     def callback(%{assigns: %{ueberauth_failure: _fails}} = conn, _params) do
@@ -36,17 +36,19 @@ defmodule ApiWeb.AuthController do
       |> redirect(to: "/")
     end
 
-    defp create(conn, changeset) do
+    defp create(conn, changeset, url_state) do
       case insert_or_update_user(changeset) do
         {:ok, user} ->
           user_id_token = Phoenix.Token.sign(conn, "user_id", user.id)
 
           Api.GoogleToken.save_auth_token(user, conn.assigns.ueberauth_auth.extra.raw_info.token)
+          
+          url = ((url_state |> Jason.decode!)["url"]) || "/"
 
           conn
           |> put_session(:user_id, user_id_token)
           |> configure_session(renew: true)
-          |> redirect(to:  "/")
+          |> redirect(to: url)
         {:error, reason} ->
           conn
           |> put_flash(:error, reason)
