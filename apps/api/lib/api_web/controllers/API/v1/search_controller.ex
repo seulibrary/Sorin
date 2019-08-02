1be59c7b53f6b5dc0ce2974f82248deb67932be9
Alex <akosarek@stedwards.edu>
2019-08-02 09:56:28 -0500

    Remove non authorized results
    A search from a non logged in user should not return collections or
    users. The results are filtered out, but the num_results remains.

--- apps/api/lib/api_web/controllers/API/v1/search_controller.ex
+++ apps/api/lib/api_web/controllers/API/v1/search_controller.ex
@@ -1,10 +1,11 @@
 defmodule ApiWeb.V1.SearchController do
   use ApiWeb, :controller
 
+  require Logger
+
   action_fallback ApiWeb.FallbackController
 
-  # Could make these just one variable, then parse out later so all are not required?
-  def search(conn, %{"query" => query, "limit" => limit, "offset" => offset} = params) do    
+  def search(conn, params) do    
     filters = if params["filters"] do 
       params["filters"] 
       |> String.slice(1..-1) # Remove first character "["
@@ -14,43 +15,88 @@ defmodule ApiWeb.V1.SearchController do
       %{}
     end
 
-    case Search.all(query, limit |> convert_params_to_int, offset |> convert_params_to_int, filters) do
-      {:ok, search} ->
-        conn
-        |> put_view(ApiWeb.SearchView)
-        |> render("index.json", %{search: search})
-      {:error, _} ->
-        conn
-        |> put_status(400)
-        |> json(%{message: "Search results not returned."})
+    limit = if params["limit"] do
+      params["limit"]
+      |> convert_params_to_int
+    else
+      25
+    end
+
+    offset = if params["offset"] do
+      params["offset"]
+      |> convert_params_to_int
+    else
+      0
+    end
+
+    {status, user_id} = Phoenix.Token.verify(conn, "user_id", conn.private[:plug_session]["user_id"], max_age: 86400)
+
+    # if type param is being passed in, it's an append (not the item_type under filters)
+    if params["type"] do
+      case params["type"] do
+        "catalog" ->
+          Logger.info"> Search: #{params["query"]}, limit: #{limit}, offset: #{offset}, filters: #{params["filters"] }, type: catalog"
+
+          conn
+          |> put_view(ApiWeb.SearchView)
+          |> remove_non_authorized_content(user_id, "catalogs.json", %{results: Search.search_catalogs(params["query"], limit, offset, filters)})
+
+        "user" ->
+          Logger.info"> Search: #{params["query"]}, limit: #{limit}, offset: #{offset}, filters: #{params["filters"] }, type: user"
+
+          conn
+          |> put_view(ApiWeb.SearchView)
+          |> remove_non_authorized_content(user_id, "users.json", %{results: Search.search_users(params["query"], limit, offset)})
+          
+        "collection" ->
+          Logger.info"> Search: #{params["query"]}, limit: #{limit}, offset: #{offset}, filters: #{params["filters"] }, type: collection"
+
+          conn
+          |> put_view(ApiWeb.SearchView)
+          |> remove_non_authorized_content(user_id, "collections.json", %{results: Search.search_collections(params["query"], limit, offset)})
+      end
+    else
+      case Search.all(params["query"], limit, offset, filters) do
+        {:ok, search} ->
+          Logger.info"> Search: #{params["query"]}, limit: #{limit}, offset: #{offset}, filters: #{params["filters"] }, type: all"
+
+          conn
+          |> put_view(ApiWeb.SearchView)
+          |> remove_non_authorized_content(user_id, "index.json", %{search: search})
+        {:error, _} ->
+          Logger.error"> Search Error: #{params["query"]}, limit: #{limit}, offset: #{offset}, filters: #{params["filters"] }, type: all"
+
+          conn
+          |> put_status(400)
+          |> json(%{message: "Search results not returned."})
+      end
     end
   end
 
-  def search(conn, %{"query" => query, "limit" => limit, "offset" => offset, "filters" => filters, "type" => type}) do
-    case type do
-      "catalog" ->
-        conn
-        |> put_view(ApiWeb.SearchView)
-        |> render("catalogs.json", %{results: Search.search_catalogs(query, limit, offset, filters)})
+  defp convert_params_to_int(input) when input |> is_integer, do: input
+  defp convert_params_to_int(input), do: input |> String.to_integer
 
-      "user" ->
-        conn
-        |> put_view(ApiWeb.SearchView)
-        |> render("users.json", %{results: Search.search_users(query, limit, offset)})
-
-      "collection" ->
-        conn
-        |> put_view(ApiWeb.SearchView)
-        |> render("collections.json", %{results: Search.search_collections(query, limit, offset)})
-    end
-  end
-
-  def search(conn, params) do
+  defp remove_non_authorized_content(conn, :missing, "catalogs.json", %{:results => _content} = data) do
     conn
-    |> put_status(400)
-    |> json(%{message: "Search results not returned."})
+    |> render("catalogs.json", data)
   end
 
-  def convert_params_to_int(input) when input |> is_integer, do: input
-  def convert_params_to_int(input), do: input |> String.to_integer
+  defp remove_non_authorized_content(conn, :missing, view, %{:results => content}) do
+    conn
+    |> render(view, %{results: nil})
+  end
+
+  defp remove_non_authorized_content(conn, :missing, view, %{:search => content}) do
+    data = content
+      |> update_in([:users, :results], fn _ -> [] end)
+      |> update_in([:collections, :results], fn _ -> [] end)
+    
+    conn
+    |> render(view, %{search: data})
+  end
+
+  defp remove_non_authorized_content(conn, user_id, view, content) do
+    conn
+    |> render(view, content)
+  end
 end
