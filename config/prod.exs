cc70db563ce732e2acac1d33993e7d5985af99c4
Casey Gibbs <caseygibbs@gmail.com>
2019-08-07 16:49:43 -0500

    Update web-facing stanzas in prod.exs for production releases

--- config/prod.exs
+++ config/prod.exs
@@ -11,9 +11,12 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :api, ApiWeb.Endpoint,
-  http: [:inet6, port: System.get_env("PORT") || 8080],
-  url: [host: "", port: 8080],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  http: [:inet6, port: 8080],
+  url: [host: "localhost", port: 8080], # This is critical for ensuring web-sockets properly authorize.
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  server: true,
+  root: ".",
+  version: Application.spec(:api, :vsn)
 
 ################ FRONTEND ####################
 # For production, don't forget to configure the url host
@@ -26,9 +29,12 @@ config :api, ApiWeb.Endpoint,
 # which you should run after static files are built and
 # before starting your production server.
 config :frontend, FrontendWeb.Endpoint,
-  http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: ""],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  http: [:inet6, port: 4000],
+  url: [host: "localhost", port: 4000], # This is critical for ensuring web-sockets properly authorize.
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  server: true,
+  root: ".",
+  version: Application.spec(:frontend, :vsn)
 
 ################ META ####################
 # Do not print debug messages in production
