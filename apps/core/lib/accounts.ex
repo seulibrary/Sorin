74f3c8b68650e540ca42b8ebea546132a8bb3bea
Akosarek <akosarek@stedwards.edu>
2019-09-20 16:14:16 -0500

    Add get user by email

--- apps/core/lib/accounts.ex
+++ apps/core/lib/accounts.ex
@@ -401,6 +401,23 @@ defmodule Core.Accounts do
   def get_user!(id), do: Repo.get!(User, id)
 
   @doc """
+  Gets a single user by email.
+  
+  Raises `Ecto.NoResultsError` if the User does not exist.
+
+  ## Examples
+
+      iex> get_user_by_email!("email@email.com")
+      %User{}
+
+      iex> get_user_by_email!("")
+      ** (Ecto.NoResultsError)
+
+
+  """
+  def get_user_by_email!(email), do: Repo.get_by!(User, email: email)
+
+  @doc """
   Creates a user.
 
   ## Examples
